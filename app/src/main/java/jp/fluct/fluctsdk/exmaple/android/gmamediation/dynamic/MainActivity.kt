9cdd0adcc7124c5b6c12624da4dedacc72295810
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2019-08-14 15:30:04 +0900

    Impl loading

--- app/src/main/java/jp/fluct/fluctsdk/exmaple/android/gmamediation/dynamic/MainActivity.kt
+++ app/src/main/java/jp/fluct/fluctsdk/exmaple/android/gmamediation/dynamic/MainActivity.kt
@@ -1,13 +1,62 @@
 package jp.fluct.fluctsdk.exmaple.android.gmamediation.dynamic
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val INTERSTITIAL_MODULE = "interstitial"
+
+    }
+
+    private lateinit var mgr: SplitInstallManager
+
+    private val listener = SplitInstallStateUpdatedListener { state ->
+        refreshStatus()
+    }
+
+    private val loadInterstitial by lazy { findViewById<Button>(R.id.load_interstitial) }
+    private val launchInterstitial by lazy { findViewById<Button>(R.id.launch_interstitial) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        mgr = SplitInstallManagerFactory.create(this)
+        mgr.registerListener(listener)
+
+        loadInterstitial.setOnClickListener {
+            requestInterstitialModule()
+        }
+
+        refreshStatus()
+    }
+
+    private fun isInstalled(module: String): Boolean {
+        return BuildConfig.DEBUG || mgr.installedModules.contains(module)
+    }
+
+    private fun refreshStatus() {
+        loadInterstitial.isEnabled = !isInstalled(INTERSTITIAL_MODULE)
+        launchInterstitial.isEnabled = !loadInterstitial.isEnabled
+    }
+
+    private fun requestInterstitialModule() {
+        mgr.startInstall(
+            SplitInstallRequest.newBuilder()
+                .addModule(INTERSTITIAL_MODULE)
+                .build()
+        )
     }
 
 }
